26dc77ec901b18e2b7888fed70dce77e0c3314c3
mhrimch <mohammed.hrimch@outlook.de>
2025-02-12 22:39:53 +0100

    first commit

---
+++ src/main/java/demo/hrimch/Main.java
@@ -0,0 +1,7 @@
+package demo.hrimch;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from CI Pipeline with Github Action, version 1!");
+    }
+}
